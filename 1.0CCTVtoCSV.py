c3d6ef63369ffeca106f4cc06f0130a3ada5f9bc
63522 <gina51226@gmail.com>
2022-11-04 10:53:29 +0800

    1.增加下載新來源XML 2.進行XML格式修正

--- 1.0CCTVtoCSV.py
+++ 1.0CCTVtoCSV.py
@@ -12,7 +12,7 @@ import xml.etree.ElementTree as ET
 
 
 def get_xml():
-    #讀取線上XML資料，將XML轉換成Python的字典格式
+    # 讀取線上XML資料，將XML轉換成Python的字典格式
     url = "http://210.241.131.244/xml/1day_cctv_config_data_https.xml"
     html = requests.get(url).text
     data = xdict.parse(html)
@@ -23,74 +23,62 @@ def get_xml():
         print(f'url={gifUrl["@url"]}')
 
 
-        
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-#    get_xml()
+    #    get_xml()
     url = "http://210.241.131.244/xml/1day_cctv_config_data_https.xml"
     data = requests.get(url)
-    # xpars = xdict.parse(data.text)
     xpars = xdict.parse(data.text)
     # print(type(xpars)) #中文
     # print(xpars)  # 中文
     # json庫dumps()是將dict轉化成json格式，loads()是將json轉化成dict格式。
     # dumps()方法的ident=1，格式化json
-    jsonstr = json.dumps(xpars, indent=1, ensure_ascii=False)  ##indent參數決定添加幾個空格
-    #Python 3中的json在做dumps操作時，會將中文轉換成unicode編碼，並以16進制方式儲存，再做逆向操作時，會將unicode編碼轉換回中文
-    ##輸出JSON
+    jsonstr = json.dumps(
+        xpars,
+        indent=1,
+        ensure_ascii=False)  # indent參數決定添加幾個空格
+    # Python 3中的json在做dumps操作時，會將中文轉換成unicode編碼，並以16進制方式儲存，再做逆向操作時，會將unicode編碼轉換回中文
+    # 輸出JSON
     with open("text.json", "w", encoding="utf-8") as f:
-      f.write(jsonstr)
+        f.write(jsonstr)
     f.close()
 
     stops = xpars["cctvinfo"]["cctv"]
     # print(f'{stops}')
     xml_list = []
-    column_name = ['dbId', 'domainId', 'eqId', 'freewayId', 'latitude', 'locationId', 'longitude', 'mainDirection',
-                   'url']
+    column_name = [
+        'dbId',
+        'domainId',
+        'eqId',
+        'freewayId',
+        'latitude',
+        'locationId',
+        'longitude',
+        'mainDirection',
+        'url']
     # list_is_none = lambda x: x[0] if x else ""
     for stop in stops:
         videoUrl = stop["videoUrlHttps"]
         # print(f'種類={type(stop["@eqId"])}\n')
-        print(f'eqId = {stop["@eqId"]} freewayId={stop["@freewayId"]} url={videoUrl["@url"]}')
-        value = (stop["@dbId"],
-                 stop["@domainId"],
-                 stop["@eqId"],
-                 stop["@freewayId"],
-                 stop["@latitude"],
-                 stop["@locationId"],
-                 stop["@longitude"],
-                 stop["@mainDirection"],
-                 videoUrl["@url"]
-                 )
+        print(
+            f'eqId = {stop["@eqId"]} freewayId={stop["@freewayId"]} pmileage={stop["@pmileage"]} url={videoUrl["@url"]}')
+        value = (
+            stop["@dbId"],
+            stop["@domainId"],
+            stop["@eqId"],
+            stop["@freewayId"],
+            stop["@latitude"],
+            stop["@locationId"],
+            stop["@longitude"],
+            stop["@mainDirection"],
+            videoUrl["@url"]
+        )
         xml_list.append(value)
 
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     xml_df.to_csv('newCCTV.csv', encoding="utf-8", index=None)
-    ##EXCL要另存為ANSI
-
-# ###lambda處理
-#     list_is_none = lambda x: x if x else ""
-#     for stop in stops:
-#         gifUrl = stop["gifUrl"]
-#         print(f'{stop["@eqId"]} mile={stop["@freewayId"]} url={gifUrl["@url"]}')
-#         value = (stop["@dbId"],
-#                  stop["@domainId"],
-#                  stop["@eqId"],
-#                  stop["@freewayId"],
-#                  stop["@latitude"],
-#                  stop["@locationId"],
-#                  stop["@longitude"],
-#                  stop["@mainDirection"],
-#                  gifUrl["@url"]
-#                  )
-#         xml_list.append(value)
-#
-#     xml_df = pd.DataFrame(xml_list, columns=column_name)
-#     xml_df.to_csv('newCCTV.csv', encoding="utf-8", index=None)
-###df2a_tp2 = df2a[df2a[ ' combineIdentifyCode ' ].map( lambda x: len(str(x).strip())>0)].copy() #識別出合單的訂單
+    # EXCL要另存為ANSI
 
 
-
-
-###encode('utf-8').decode('utf-8')
-###encode('utf-8').decode('unicode_escape')
+# encode('utf-8').decode('utf-8')
+# encode('utf-8').decode('unicode_escape')
--- CrawlCCTV.py
+++ CrawlCCTV.py
@@ -9,27 +9,30 @@ import requests
 import xmltodict
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-def get_xml():
-    #讀取線上XML資料，將XML轉換成Python的字典格式
-    url = "http://210.241.131.244/xml/1day_cctv_config_data_https.xml"
+def get_xmltodict(url):
+    # 讀取線上XML資料，將XML轉換成Python的字典格式
     html = requests.get(url).text
+
     data = xmltodict.parse(html)
     stops = data["cctvinfo"]["cctv"]
-    print(f'{stops}')
-    # for stop in stops:
-    #     print(f'{stop["@eqId"]} mile={stop["@pmileage"]}')
-    #     gifUrl = stop["gifUrl"]
-    #     print(f'url={gifUrl["@url"]}')
-    # stops = data["cctvinfo"]["cctv"]
-    # for stop in stops:
-    #     print(f'Hi,{stop["@eqId"]}, {stop["@pmileage"]}')
+
+    for stop in stops:
+        videoUrl = stop["videoUrlHttps"]
+        # print(f'種類={type(stop["@eqId"])}\n')
+        print(
+            f'eqId = {stop["@eqId"]} freewayId={stop["@freewayId"]} pmileage={stop["@pmileage"]} url={videoUrl["@url"]}')
+
+
+def download_xml(url):
+    response = requests.get(url)
+    Result_Name = "1day_cctv_config_data.xml"
+    with open(Result_Name, 'wb') as file:
+        file.write(response.content)
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    get_xml()
 
+    URL = "http://210.241.131.244/xml/1day_cctv_config_data_https.xml"
 
+    # get_xmltodict(URL)
+    download_xml(URL)
