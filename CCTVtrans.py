3c30845a5a36ccce0e91d91b76d761531282ba9b
63522 <gina51226@gmail.com>
2022-11-10 15:21:54 +0800

    給""會出現locationpath錯誤，推測是因為屬性不能為STR

--- CCTVtrans.py
+++ CCTVtrans.py
@@ -47,14 +47,26 @@ if __name__ == '__main__':
     for stop in stops:
         Info = ET.Element(
             'Info', {
-                "cctvid": "nfb"+str(stop["@eqId"]),
-                "roadsection": "",
-                "locationpath": "",
-                "startlocationpoint": "",
-                "endlocationpoint": "",
-                "px": str(stop["@longitude"]),
-                "py": str(stop["@latitude"])})
+                "cctvid":"nfb"+str(stop["@eqId"]),
+                "roadsection":"0",
+                "locationpath":"0",  ##給""會出現locationpath錯誤，推測是因為屬性
+                "startlocationpoint":"0",
+                "endlocationpoint":"0",
+                "px":str(stop["@longitude"]),
+                "py":str(stop["@latitude"])})
         Infos.append(Info)
 
     tree = ET.ElementTree(root)
+
     tree.write(os.path.join(os.path.dirname(__file__), "newCCTVto1.1.xml"))
+
+
+"""JAVA設定的屬性如下：
+        # String cctvid;
+		# String roadsection;
+		# int locationpath;
+		# int startlocationpoint;
+		# int endlocationpoint;
+		# float px;
+		# float py;
+"""
